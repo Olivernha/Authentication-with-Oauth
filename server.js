4ab6bbf06acd54fd6059746e36e03142ebdbfaff
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-05-30 17:34:07 +0630

    authenticating endpoint with middleware

--- server.js
+++ server.js
@@ -6,7 +6,27 @@ const PORT = 3000;
 const app = express();
 
 app.use(helmet());
-app.use
+function checkLoggedIn(req,res,next){
+    const isLoggedIn = true;
+    if(!isLoggedIn){
+        return res.status(401).json({
+            error: "You must be logged in to view this page"
+        })
+    }
+    next();
+}
+app.get('/auth/google',(req,res)=>{
+
+})
+app.get('/auth/google/callback',(req,res)=>{
+    
+})
+app.get('/auth/logout',(req,res)=>{
+    
+})
+app.get('/secret',checkLoggedIn,(req,res)=>{
+    return res.send('secret page');
+});
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
