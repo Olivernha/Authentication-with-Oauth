92abe7617ae6b85086d2c43ab2da6717a40917df
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-05-31 20:59:44 +0630

    setting up cookie-session config

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 const passport = require("passport");
+const cookieSession = require('cookie-session');
 const { Strategy } = require("passport-google-oauth20");
 const PORT = 3000;
 require("dotenv").config();
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1 : process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2 : process.env.COOKIE_KEY_2
 };
 const AUTH_OPTIONS = {
   callbackURL: "/auth/google/callback",
@@ -20,8 +23,14 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 }
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 const app = express();
-app.use(passport.initialize());
 app.use(helmet());
+app.use(cookieSession({
+  name: 'session',
+  maxAge : 24 * 60 * 60 * 1000,
+  keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}))
+app.use(passport.initialize());
+
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true;
   if (!isLoggedIn) {
